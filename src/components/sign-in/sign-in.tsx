01ef814598ec61fbd731413a6f2873d0ab5c5de7
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-01-10 15:58:40 +0200

    adding validation for sign in

--- src/components/sign-in/sign-in.tsx
+++ src/components/sign-in/sign-in.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { SubmitHandler } from 'react-hook-form/dist/types';
 import { useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../consts';
 import { useAppDispatch } from '../../hooks';
@@ -6,18 +7,20 @@ import { loginAction } from '../../store/actions/api-actions';
 import { UserData } from '../../types/user.model';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+// import { ChangeEvent, FormEvent, useState } from 'react';
 
 function SignIn() {
-  const [userAccountData, setUserAccountData] = useState<UserData>({
-    email: '',
-    password: '',
-  });
+  const { register, formState: {errors}, handleSubmit } = useForm<UserData>();
+  // const [userAccountData, setUserAccountData] = useState<UserData>({
+  //   email: '',
+  //   password: '',
+  // });
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(loginAction(userAccountData));
+  const handleSubmitForm: SubmitHandler<UserData> = (userData) => {
+    // e.preventDefault();
+    dispatch(loginAction(userData));
     navigate(AppRoute.Main);
   };
 
@@ -25,22 +28,24 @@ function SignIn() {
     <div className="user-page">
       <Header />
       <div className="sign-in user-page__content">
-        <form onSubmit={handleSubmit} action="#" className="sign-in__form">
+        <form onSubmit={handleSubmit(handleSubmitForm)} className="sign-in__form">
           <div className="sign-in__fields">
             <div className="sign-in__field">
               <input
                 className="sign-in__input"
                 type="email"
                 placeholder="Email address"
-                name="user-email"
+                // name="user-email"
                 id="user-email"
-                value={userAccountData.email}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setUserAccountData({
-                    ...userAccountData,
-                    email: e.target.value,
-                  })}
+                // value={userAccountData.email}
+                // onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                //   setUserAccountData({
+                //     ...userAccountData,
+                //     email: e.target.value,
+                //   })}
+                {...register('email', {required: true})}
               />
+              <div style={{height: 40}}></div>
               <label
                 className="sign-in__label visually-hidden"
                 htmlFor="user-email"
@@ -53,14 +58,15 @@ function SignIn() {
                 className="sign-in__input"
                 type="password"
                 placeholder="Password"
-                name="user-password"
+                // name="user-password"
                 id="user-password"
-                value={userAccountData.password}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setUserAccountData({
-                    ...userAccountData,
-                    password: e.target.value,
-                  })}
+                // value={userAccountData.password}
+                // onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                //   setUserAccountData({
+                //     ...userAccountData,
+                //     password: e.target.value,
+                //   })}
+                {...register('password')}
               />
               <label
                 className="sign-in__label visually-hidden"
